21d49533b4eb16f7c1da606e28dee38b965e9a31
consortWebsite <mohdsa@iul.ac.in>
2023-09-13 16:57:58 +0530

    Added code till navbar and hero section !

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import Truck from "../assets/icons/Truck";
+import FaceBook from "../assets/icons/FaceBook";
+import LinkedIn from "../assets/icons/LinkedIn";
+import Twitter from "../assets/icons/Twitter";
+import Insta from "../assets/icons/Insta";
+import "./Navbar.css";
+import Star from "../assets/icons/Star";
+import ExpandMenu from "../assets/icons/ExpandMenu";
+import Cross from "../assets/icons/Cross";
+import heroImg from "../assets/imgs/heroImg.png";
+
+const navLinks = [
+  { name: "Home", id: "home" },
+  { name: "About", id: "about" },
+  {
+    name: "Our Products",
+    id: "product",
+    child: [
+      { name: "Product 1", id: "p1" },
+      { name: "Product 2", id: "p2" },
+      { name: "Product 3", id: "p3" },
+      { name: "Product 4", id: "p4" },
+    ],
+  },
+  { name: "Contact Us", id: "contact" },
+];
+
+const Navbar = () => {
+  // state for handeling the active link
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+  const [isExapndable, setIsExpandable] = useState<boolean>(false);
+
+  //   Method for handeling the click of link
+  const openLink = (id: string) => {
+    if (activeLink === id) {
+      setActiveLink(null);
+    } else setActiveLink(id);
+  };
+
+  return (
+    <>
+      <div className="announcement--bar">
+        <span className="truck--container">
+          <Truck /> Free Delivery |{" "}
+          <a href="#" className="link">
+            Return Policy
+          </a>
+        </span>
+        <div className="right--container">
+          <a href="#" className="link login">
+            Login
+          </a>
+          <div className="links--container">
+            <a href="#" className="link follow">
+              Follow US
+            </a>
+            <a href="#" className="link">
+              <FaceBook />
+            </a>
+            <a href="#" className="link">
+              <LinkedIn />
+            </a>
+            <a href="#" className="link">
+              <Twitter />
+            </a>
+            <a href="#" className="link">
+              <Insta />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="main--container">
+        <div className="heading--container">
+          <div className="heading--container--left">
+            <h2>Shopkart</h2>
+          </div>
+          <div className="heading--container--right">
+            <h4>WISHLIST (0) </h4>
+            <h4>BAG (0) </h4>
+          </div>
+          <div
+            className="expandable--column"
+            onClick={() => {
+              setIsExpandable(!isExapndable);
+            }}
+          >
+            {!isExapndable ? <ExpandMenu /> : <Cross />}
+          </div>
+        </div>
+        <div className="designer--break">
+          <div className="star--container">
+            <Star />
+          </div>
+        </div>
+        <div
+          className={`${
+            isExapndable
+              ? "navbar--expandable--conatiner"
+              : "navbar--links--container"
+          }`}
+        >
+          {navLinks.map((item) => (
+            <div
+              key={item.id}
+              className={`${
+                activeLink === item.id
+                  ? "active--navbar--links"
+                  : "navbar--links"
+              }`}
+            >
+              <div
+                className={`${activeLink === item.id ? "active--link" : ""}`}
+                onClick={() => openLink(item.id)}
+              >
+                {item.name.toLocaleUpperCase()}
+              </div>
+              {item.child && activeLink === item.id && (
+                <div className="sublink--container">
+                  {item.child.map((sublink) => (
+                    <div key={sublink.id} className="navbar--sublinks">
+                      {sublink.name}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="hero--image--section">
+          <div className="hero--image--heading--conatiner">
+            <h1 className="image--heading">Fresh</h1>
+            <h1 className="image--heading hollow--design">2022</h1>
+            <h1 className="image--heading">Look</h1>
+          </div>
+          <div className="hero--image--container">
+            <img src={heroImg} alt="heroimg.png" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/assets/icons/Cross.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Cross = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19 5L5 19M5.00003 5L19 19"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <rect x="0.5" y="0.5" width="23" height="23" stroke="white" />
+    </svg>
+  );
+};
+
+export default Cross;
